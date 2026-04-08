72ee85e847d04b3b177079036f9eb66e9c2b02b5
field cady <field.cady@gmail.com>
2026-04-08 13:02:16 -0700

    Switch map provider from Mapbox to Leaflet with OSM CartoDB tiles

--- myscripts.js
+++ myscripts.js
@@ -9,22 +9,54 @@ var DEFAULT_DATA = {"lakes": [
   {"overabundant":true, "starting":true, "name": "Bannock - Middle", "url": "https://wdfw.wa.gov/fishing/locations/high-lakes/bannock-middle", "elevation": 5929.0, "county": "Chelan", "lat": 48.260666, "lon": -120.972715, "area": "6.90 Acres"}
 ]}
 
-//var data = null;
 var mymap = null;
 var markers = [];
+
+// Colored icons for Leaflet
+var redIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+var blueIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+var greyIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-grey.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+var violetIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
 
 // Functions for populating the map
 
 var initializeMap = function() {
-  mapboxgl.accessToken = "pk.eyJ1IjoiZmllbGRjYWR5IiwiYSI6ImNqd3Rmb2d3bjBkMDA0OW5yamYxNnRwdGwifQ.kBilx8iMkTn8RUyrO7ZHGA";
-  mymap = new mapboxgl.Map({
-    container: 'mapid',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    center: [-120.661, 47.596],
-    zoom: 5,
-    maxZoom: 13,
-    maxBounds: [[-130, 45], [-110, 50]]
-  });
+  mymap = L.map('mapid').setView([47.596, -120.661], 7);
+  
+  L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+  }).addTo(mymap);
 }
 
 var downloadDataAndRender = function(url) {
@@ -32,72 +64,56 @@ var downloadDataAndRender = function(url) {
     xhr.open('GET', url, true);
     xhr.responseType = 'json';
     xhr.onload = function() {
-      data=xhr.response;
-      renderData(data);
+      if (xhr.status === 200) {
+        data = xhr.response;
+        renderData(data);
+      }
     };
     xhr.send();
 };
 
 var renderData = function(dat) {
-  showTimestamp(dat["timestamp"]);
+  if (dat["timestamp"]) {
+    showTimestamp(dat["timestamp"]);
+  }
   addLakesToMap(dat["lakes"]);
   updateMarkers();
 }
 
-var isMobile = function() {
-  if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    return true;
-  }else{
-    return false;
-  }
-}
-
 var addLakesToMap = function(lakes) {
-  if (isMobile()) {
-      scl = 2
-  } else {
-      scl = 1
-  }
-  for (i=0; i<lakes.length; i++) {
-    lk = lakes[i];
-    //console.log(lk);
-    if (lk['starting'] & lk['overabundant']) {
-      color='violet'
-    } else if (lk['starting'] & (!lk['overabundant'])) {
-      color='red'
-    } else if ((!lk['starting']) & lk['overabundant']) {
-      color='blue'
-    } else {
-      color='grey'
+  for (var i=0; i<lakes.length; i++) {
+    var lk = lakes[i];
+    var icon = greyIcon;
+    
+    if (lk['starting'] && lk['overabundant']) {
+      icon = violetIcon;
+    } else if (lk['starting'] && (!lk['overabundant'])) {
+      icon = redIcon;
+    } else if ((!lk['starting']) && lk['overabundant']) {
+      icon = blueIcon;
     }
-    //m = new mapboxgl.Marker({color: color, size: 'small'})
-    m = new mapboxgl.Marker({color: color, scale: scl})
-    console.log('FOO')
-    m.setLngLat([lk['lon'], lk['lat']]);
-    //m.classList.add('marker');
-    var popup = new mapboxgl.Popup().setHTML(lake2marker_html(lk));
-    m.setPopup(popup);
-    m.addTo(mymap);
-    //
+    
+    var m = L.marker([lk['lat'], lk['lon']], {icon: icon});
+    m.bindPopup(lake2marker_html(lk));
+    
     m.lake = lk;
     lk.marker = m;
     markers.push(m);
-  };
+  }
 }
 
 
 // Helper utility functions
 
 var lake2marker_html = function(lk) {
-  clickable_name = '<a target="_blank" href="'+lk['url']+'">'+lk['name']+'</a>'
-  elevation = '<p>Elevation: '+String(Math.round(lk['elevation']))+'ft'
-  county = '<p>County: '+lk['county']+'</p>'
-  size = '<p>Size: '+String(lk['area'])+'</p>'
+  var clickable_name = '<a target="_blank" href="'+lk['url']+'">'+lk['name']+'</a>'
+  var elevation = '<p>Elevation: '+String(Math.round(lk['elevation']))+'ft'
+  var county = '<p>County: '+lk['county']+'</p>'
+  var size = '<p>Size: '+String(lk['area'])+'</p>'
   return clickable_name+elevation+county+size;
 }
 
 var getFilterFunction = function() {
-  console.log('in getFilterFunction')
   // Reads current filter settings and returns a function that says whether
   // a lake passes those filters
 
@@ -105,86 +121,89 @@ var getFilterFunction = function() {
   var show_overabundant = document.getElementById('overabundant_filter').checked;
   var show_starting = document.getElementById('starting_filter').checked;
   var show_other = document.getElementById('other_filter').checked;
-  type_filter = function(lk) {
-    if (lk['starting'] & show_starting) return true;
-    if (lk['overabundant'] & show_overabundant) return true;
-    return show_other;
+  
+  var type_filter = function(lk) {
+    if (lk['starting'] && show_starting) return true;
+    if (lk['overabundant'] && show_overabundant) return true;
+    if (!lk['starting'] && !lk['overabundant'] && show_other) return true;
+    return false;
   }
+  
   // Elevation
   var elevation_filter_value = document.getElementById('elevation_filter').value
+  var elevation_filter;
   if (elevation_filter_value=='any') {
     elevation_filter = function(el){return true}
   } else if (elevation_filter_value=='<4000') {
     elevation_filter = function(el){return el<4000}
   } else if (elevation_filter_value=='4000-5000') {
-    elevation_filter = function(el){return (4000<=el & el<=5000)}
+    elevation_filter = function(el){return (4000<=el && el<=5000)}
   } else if (elevation_filter_value=='5000-6000') {
-    elevation_filter = function(el){return (5000<=el & el<=6000)}
+    elevation_filter = function(el){return (5000<=el && el<=6000)}
   } else if (elevation_filter_value=='>6000') {
     elevation_filter = function(el){return el>6000}
   }
+  
   // Size
   var size_filter_value = document.getElementById('size_filter').value
+  var size_filter;
   if (size_filter_value=='any') {
     size_filter = function(el){return true}
   } else if (size_filter_value=='<5') {
-    size_filter = function(el){return el<5}
+    size_filter = function(el){return parseFloat(el)<5}
   } else if (size_filter_value=='5-10') {
-    size_filter = function(el){return (5<=el & el<=10)}
+    size_filter = function(el){var val = parseFloat(el); return (5<=val && val<=10)}
   } else if (size_filter_value=='>10') {
-    size_filter = function(el){return 10<el}
+    size_filter = function(el){return 10<parseFloat(el)}
   }
+  
   // County
   var county_filter_value = document.getElementById('county_filter').value
+  var county_filter;
   if (county_filter_value=='any') {
     county_filter = function(cty){return true}
   } else {
     county_filter = function(cty){return cty==county_filter_value}
   }
-  //
+  
   return function(lk) {
-    return elevation_filter(lk.elevation) & size_filter(lk.acres) & county_filter(lk.county) & type_filter(lk)
+    return elevation_filter(lk.elevation) && size_filter(lk.area) && county_filter(lk.county) && type_filter(lk)
   }
 }
 
 // Top-level functions below this line
 
 var updateMarkers = function() {
-  filter_func = getFilterFunction()
-  for (i=0; i<markers.length; i++) {
-    m = markers[i];
-    lk = m.lake;
+  var filter_func = getFilterFunction()
+  for (var i=0; i<markers.length; i++) {
+    var m = markers[i];
+    var lk = m.lake;
     if (filter_func(lk)) {
-      m.addTo(mymap);
+      if (!mymap.hasLayer(m)) {
+        m.addTo(mymap);
+      }
     } else {
-      m.remove(mymap);
+      if (mymap.hasLayer(m)) {
+        mymap.removeLayer(m);
+      }
     }
-  };
+  }
 }
 
 var showTimestamp = function(timestamp) {
-  timestamp_div = document.getElementById("last_update_timestamp");
-  var content = document.createTextNode(timestamp);
-  timestamp_div.appendChild(content);
-}
-
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
+  var timestamp_div = document.getElementById("last_update_timestamp");
+  if (timestamp_div) {
+    timestamp_div.innerHTML = "The data was last updated at: " + timestamp;
   }
 }
 
 initializeMap()
 
 if (location.origin === "file://") {
-    //data = DEFAULT_DATA
     renderData(DEFAULT_DATA);
   } else {
     downloadDataAndRender("data/overabundant_lakes.json");
     downloadDataAndRender("data/starting_lakes.json");
-    sleep(500);
+    // Removed the sleep(500) and manual third call to be more robust
     downloadDataAndRender("data/normal_lakes.json");
 }
